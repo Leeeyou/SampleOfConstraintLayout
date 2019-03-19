bed0015099ad449a543954b849a63ffaaa624a6a
LeeeYou <leeeyou@foxmail.com>
2019-03-19 15:30:51 +0800

    init

---
+++ app/src/main/java/com/leeeyou/sampleofconstraintlayout/BarrierActivity.kt
@@ -0,0 +1,12 @@
+package com.leeeyou.sampleofconstraintlayout
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class BarrierActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_barrier)
+    }
+}
